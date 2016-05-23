8b3d4d03cb1ee84add5b11e0806a9029f70c3489
Edward Viaene <ward@in4it.io>
2016-05-23 12:58:03 +0200

    vpc_setup only

--- nat.tf
+++
@@ -1,9 +0,0 @@
-# nat gw
-resource "aws_eip" "nat" {
-  vpc      = true
-}
-resource "aws_nat_gateway" "nat-gw" {
-  allocation_id = "${aws_eip.nat.id}"
-  subnet_id = "${aws_subnet.main-public-1.id}"
-  depends_on = ["aws_internet_gateway.main-gw"]
-}
