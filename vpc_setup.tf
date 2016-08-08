94a2d0c0e40ddefcbad6b9f55dca728c34d114cc
Edward Viaene <ward.viaene@gmail.com>
2016-08-08 10:27:36 +0200

    Update vpc_setup.tf
    enable_dns_hostnames true

--- vpc_setup.tf
+++ vpc_setup.tf
@@ -3,7 +3,7 @@ resource "aws_vpc" "main" {
     cidr_block = "10.0.0.0/16"
     instance_tenancy = "default"
     enable_dns_support = "true"
-    enable_dns_hostnames = "false"
+    enable_dns_hostnames = "true"
     enable_classiclink = "false"
     tags {
         Name = "main"
